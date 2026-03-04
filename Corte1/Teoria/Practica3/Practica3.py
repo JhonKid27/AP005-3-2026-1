b2f6637b00c53c98ccedf102152dcc3dc8b82e07
JhonNiño135 <jhonff.cuenta02@gmail.com>
2026-03-04 15:16:06 -0500

    Create Practica3.py

---
+++ Corte1/Teoria/Practica3/Practica3.py
@@ -0,0 +1,15 @@
+while true:
+a= input("porfavor ingrese un valor: ")
+print(a)
+
+mod= int(a)%2
+print(mod)
+if (mod==0):
+    print("Par")
+else:
+    print("Impar")
+
+b=input("ingrese 1 para continuar, 0 para finalizar")
+print(b)
+    if(b==0):
+    break:
